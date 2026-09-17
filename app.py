cc6ff9dbfdd2f66821e635b21dea3fd29a623303
AQSHAFATHIMA <aqshafathima.i2024@vitstudent.ac.in>
2026-09-17 15:12:20 +0530

    Update total users count in report generation

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 with open("report.txt","w")as f:
   f.write("Online Examination and Evalution System Report\n")
-  f.write("Total users:120\n")
+  f.write("Total users:200\n")
   f.write("Active Sessions:45\n")
   f.write("Examinations Conducted:80\n")
   f.write("Evaluations Completed:75\n")
